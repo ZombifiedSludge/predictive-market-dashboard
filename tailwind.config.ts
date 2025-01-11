978d69f992029be2f4cb2fa96752d46b54e34afd
ZombifiedSludge <bowenadj@gmail.com>
2025-01-11 02:45:18 -0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,15 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',  // Enable class-based dark mode
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: {
+          900: 'rgb(0, 27, 83)'  // Dark mode gradient color
+        }
+      }
+    },
   },
   plugins: [],
 };
