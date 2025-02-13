58b9f3e0659e2281eaaa914e7c1c5b2a7cc88ef0
ZombifiedSludge <bowenadj@gmail.com>
2025-02-12 20:06:16 -0500

    Create Blog.tsx

---
+++ src/widgets/Blog.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'solid-js';
+
+const Blog: Component = () => {
+  return (
+    <div className="container mx-auto px-6 py-8">
+      {/* Your blog content will go here later */}
+    </div>
+  );
+};
+
+export default Blog;
